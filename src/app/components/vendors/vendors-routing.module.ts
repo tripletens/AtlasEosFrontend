558228a1c26c9782c1544a95a501ac04733d6f4b
achawayne3031 <59153600+achawayne3031@users.noreply.github.com>
2022-06-20 18:08:42 +0100

    update

--- src/app/components/vendors/vendors-routing.module.ts
+++ src/app/components/vendors/vendors-routing.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { VendorsComponent } from './vendors.component';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { VendorsComponent } from './vendors.component'
 
-const routes: Routes = [{ path: '', component: VendorsComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+
+  {
+    path: '',
+    component: VendorsComponent,
+    children: [],
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class VendorsRoutingModule { }
+export class VendorsRoutingModule {}
