fa303fc212e82664dad3b2dd110bb0f13ae46520
achawayne3031 <59153600+achawayne3031@users.noreply.github.com>
2022-06-21 16:00:56 +0100

    update

--- src/app/components/admin/all-seminars/all-seminars.component.ts
+++ src/app/components/admin/all-seminars/all-seminars.component.ts
@@ -1,4 +1,19 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, ViewChild } from '@angular/core'
+import { HttpRequestsService } from 'src/app/core/services/http-requests.service'
+import { ToastrService } from 'ngx-toastr'
+import { MatPaginator } from '@angular/material/paginator'
+import { MatTableDataSource } from '@angular/material/table'
+import Swal from 'sweetalert2'
+
+declare var $: any
+
+export interface PeriodicElement {
+  seminarDate: string
+  scheduledTime: string
+  vendorName: string
+  topic: string
+  link: string
+}
 
 @Component({
   selector: 'app-all-seminars',
@@ -6,10 +21,153 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./all-seminars.component.scss'],
 })
 export class AllSeminarsComponent implements OnInit {
-  tableView = true
-  loader = false
+  tableView = false
+  loader = true
+  allVendor: any
+  loaderData = [9, 8, 6]
+  incomingData: any
 
-  constructor() {}
+  displayedColumns: string[] = [
+    'seminar_date',
+    'scheduled_time',
+    'vendor_name',
+    'topic',
+    'link',
+    'action',
+  ]
 
-  ngOnInit(): void {}
+  dataSource = new MatTableDataSource<PeriodicElement>()
+  @ViewChild(MatPaginator) paginator!: MatPaginator
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator
+  }
+
+  ngOnInit(): void {
+    this.getAllSeminars()
+  }
+
+  pageSizes = [3, 5, 7]
+
+  constructor(
+    private postData: HttpRequestsService,
+    private toastr: ToastrService,
+  ) {}
+
+  async removeVendor(index: any) {
+    let confirmStatus = await this.confirmBox()
+
+    if (confirmStatus) {
+      $('#remove-icon-' + index).css('display', 'none')
+      $('#remove-loader-' + index).css('display', 'inline-block')
+
+      this.postData
+        .httpGetRequest('/deactivate-dealer-user/' + index)
+        .then((result: any) => {
+          $('#remove-icon-' + index).css('display', 'inline-block')
+          $('#remove-loader-' + index).css('display', 'none')
+
+          if (result.status) {
+            this.toastr.success('Successful', result.message)
+            this.getAllSeminars()
+          } else {
+            this.toastr.error('Something went wrong', 'Try again')
+          }
+        })
+        .catch((err) => {
+          this.toastr.error('Something went wrong', 'Try again')
+        })
+    } else {
+    }
+  }
+
+  async confirmBox() {
+    return await Swal.fire({
+      title: 'You Are About To Remove This Vendor User',
+      text: '',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes',
+      cancelButtonText: 'Cancel',
+    }).then((result) => {
+      if (result.value) {
+        return true
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        return false
+      } else {
+        return false
+      }
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value
+    this.incomingData.vendor_name = filterValue.trim().toLowerCase()
+
+    this.dataSource = this.filterArray('*' + filterValue)
+
+    //console.log(res)
+  }
+
+  filterArray(expression: string) {
+    var regex = this.convertWildcardStringToRegExp(expression)
+    //console.log('RegExp: ' + regex);
+    return this.incomingData.filter(function (item: any) {
+      return regex.test(item.full_name)
+    })
+  }
+
+  escapeRegExp(str: string) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+  }
+
+  convertWildcardStringToRegExp(expression: string) {
+    var terms = expression.split('*')
+
+    var trailingWildcard = false
+
+    var expr = ''
+    for (var i = 0; i < terms.length; i++) {
+      if (terms[i]) {
+        if (i > 0 && terms[i - 1]) {
+          expr += '.*'
+        }
+        trailingWildcard = false
+        expr += this.escapeRegExp(terms[i])
+      } else {
+        trailingWildcard = true
+        expr += '.*'
+      }
+    }
+
+    if (!trailingWildcard) {
+      expr += '.*'
+    }
+
+    return new RegExp('^' + expr + '$', 'i')
+  }
+
+  getAllSeminars() {
+    this.postData
+      .httpGetRequest('/get-all-seminar')
+      .then((result: any) => {
+        console.log(result)
+
+        if (result.status) {
+          this.allVendor = result.data
+          // this.dataSource = result.data
+          this.loader = false
+          this.tableView = true
+          this.incomingData = result.data
+          this.dataSource = new MatTableDataSource<PeriodicElement>(result.data)
+
+          this.dataSource.paginator = this.paginator
+        } else {
+          // this.toastr.error(result.message, 'Try again')
+        }
+      })
+      .catch((err) => {
+        // this.toastr.error('Try again', 'Something went wrong')
+      })
+  }
 }
