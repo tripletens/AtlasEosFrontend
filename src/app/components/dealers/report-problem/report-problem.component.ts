e2878c782e342b9211dbcfcdd5b3eeb5c91b3828
EDDIE <70353768+Eddie-ui@users.noreply.github.com>
2022-07-04 19:17:21 +0100

    update

--- src/app/components/dealers/report-problem/report-problem.component.ts
+++ src/app/components/dealers/report-problem/report-problem.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-report-problem',
   templateUrl: './report-problem.component.html',
-  styleUrls: ['./report-problem.component.scss']
+  styleUrls: ['./report-problem.component.scss'],
 })
 export class ReportProblemComponent implements OnInit {
+  @ViewChild('subject') subject!: ElementRef;
+  @ViewChild('photo') photo!: ElementRef;
+  @ViewChild('description') description!: ElementRef;
+  imgData: any;
+  imgURL2: any;
+  formError = false;
+  formLoader = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
+ 
 }
