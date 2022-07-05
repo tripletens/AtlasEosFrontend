820a8c76fb1457eed021483b9ed492ea4e8a2c32
EDDIE <70353768+Eddie-ui@users.noreply.github.com>
2022-07-05 08:18:12 +0100

    update

--- src/app/components/dealers/report-problem/report-problem.component.ts
+++ src/app/components/dealers/report-problem/report-problem.component.ts
@@ -1,4 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { HttpRequestsService } from 'src/app/core/services/http-requests.service';
+import { TokenStorageService } from 'src/app/core/services/token-storage.service';
 
 @Component({
   selector: 'app-report-problem',
@@ -12,9 +15,55 @@ export class ReportProblemComponent implements OnInit {
   imgData: any;
   imgURL2: any;
   formError = false;
+  uploadedFile: any;
   formLoader = false;
-  constructor() {}
+  responseError: any;
+  constructor(
+    private token: TokenStorageService,
+    private postData: HttpRequestsService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {}
- 
+  fileUpload() {
+    this.uploadedFile = this.photo.nativeElement?.files[0];
+
+    // console.log('file changefd', this.uploadedFile, "tojken",  this.token.getUser().account_id,);
+  }
+  submitReport() {
+    this.formLoader = false;
+    let sub = this.subject.nativeElement.value!;
+    let desc = this.description.nativeElement.value!;
+    let img = this.uploadedFile!;
+    if (sub && desc) {
+      this.formLoader = true;
+      this.formError = false;
+      let formData = {
+        subject: sub,
+        description: desc,
+        photo: img,
+        role: 'dealer',
+        dealer_id: this.token.getUser().account_id,
+      };
+      console.log('formdata', sub, desc, img, formData);
+      // this.postData
+      //   .httpPostRequest('/create-report',formData)
+      //   .then((result: any) => {
+      //     this.formLoader = false;
+      //     if (result.status) {
+      //       console.log('result', result);
+      //     } else {
+      //       this.toastr.error('', `Something went wrong`);
+      //     }
+      //   })
+      //   .catch((err) => {
+      //     this.toastr.error('Something went wrong', `Network Error`);
+      //     this.formLoader = false;
+      //   });
+    } else {
+      this.formLoader = false;
+      this.formError = true;
+      console.log('else info', sub, desc, img);
+    }
+  }
 }
