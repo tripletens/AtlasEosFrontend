f9a9db60f7d2f37408517084fa2a723f4adb63ba
achawayne3031 <59153600+achawayne3031@users.noreply.github.com>
2022-07-07 01:45:09 +0100

    update

--- src/app/components/vendors/report-problem/report-problem.component.ts
+++ src/app/components/vendors/report-problem/report-problem.component.ts
@@ -1,15 +1,126 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core'
+import { TokenStorageService } from 'src/app/core/services/token-storage.service'
+import { HttpRequestsService } from 'src/app/core/services/http-requests.service'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { ToastrService } from 'ngx-toastr'
 
 @Component({
   selector: 'app-report-problem',
   templateUrl: './report-problem.component.html',
-  styleUrls: ['./report-problem.component.css']
+  styleUrls: ['./report-problem.component.scss'],
 })
 export class ReportProblemComponent implements OnInit {
+  tableView = true
+  loader = false
+  userData: any
+  faqData: any
+  faqForm!: FormGroup
+  manualChecker = false
 
-  constructor() { }
+  btnText = true
+  btnLoader = false
+  selectedfileName!: any
+  @ViewChild('attachmentFile') csvFile!: ElementRef
+  attachmentDataFile: any
+  fileSelected = false
 
-  ngOnInit(): void {
+  constructor(
+    private tokenData: TokenStorageService,
+    private httpServer: HttpRequestsService,
+    private fb: FormBuilder,
+    private toastr: ToastrService,
+  ) {
+    this.userData = tokenData.getUser()
   }
 
+  ngOnInit(): void {
+    this.buildFaqForm()
+  }
+
+  callUploadInput() {
+    this.csvFile.nativeElement.click()
+  }
+
+  fileCsvUpload(files: any) {
+    if (files.length === 0) return
+    var mimeType = files[0].type
+    // if (mimeType !== 'application/*') {
+    //   this.toastr.error(
+    //     'File type not supported, upload a CSV file',
+    //     `Upload Error`,
+    //   )
+    //   return
+    // }
+
+    this.selectedfileName = files[0].name
+    this.fileSelected = true
+    this.attachmentDataFile = files
+  }
+
+  submit() {
+    if (this.faqForm.status == 'VALID') {
+      this.manualChecker = false
+
+      this.btnText = false
+      this.btnLoader = true
+
+      let fd = new FormData()
+      fd.append('subject', this.faqForm.value.subject)
+      fd.append('description', this.faqForm.value.description)
+
+      if (this.fileSelected) {
+        fd.append('csv', this.attachmentDataFile[0])
+      }
+
+      this.httpServer
+        .uploadFile('/create-report', fd)
+        .then((result) => {
+          this.btnText = true
+          this.btnLoader = false
+          if (result.status) {
+            this.toastr.success('Csv File Uploaded successfully', `Success`)
+          } else {
+            this.toastr.error(
+              'Somethin went wrong, Try again',
+              `Uploading Error`,
+            )
+          }
+        })
+        .catch((err) => {
+          this.btnText = true
+          this.btnLoader = false
+          this.toastr.error('Somethin went wrong, Try again', `Uploading Error`)
+        })
+    } else {
+      this.manualChecker = true
+      // this.toastr.error('Try again', 'Something went wrong')
+    }
+  }
+
+  buildFaqForm(): void {
+    this.faqForm = this.fb.group({
+      subject: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+    })
+  }
+
+  get faqFormControls() {
+    return this.faqForm.controls
+  }
+
+  getErrorMessage(instance: string) {
+    if (
+      instance === 'subject' &&
+      this.faqFormControls.subject.hasError('required')
+    ) {
+      return 'enter the report subject'
+    } else if (
+      instance === 'description' &&
+      this.faqFormControls.description.hasError('required')
+    ) {
+      return 'enter the report description'
+    } else {
+      return
+    }
+  }
 }
