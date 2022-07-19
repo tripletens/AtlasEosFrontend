c44c07a4d03aa60cc5dd7c3ff51617b67c501ec6
achawayne3031 <59153600+achawayne3031@users.noreply.github.com>
2022-07-19 22:47:03 +0100

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //// url: 'https://atlas-eos-backend-app-k4s5v.ondigitalocean.app/api',
-  url: 'http://127.0.0.1:8000/api',
+  url: 'https://atlas-eos-backend-app-k4s5v.ondigitalocean.app/api',
+  ///url: 'http://127.0.0.1:8000/api',
 }
